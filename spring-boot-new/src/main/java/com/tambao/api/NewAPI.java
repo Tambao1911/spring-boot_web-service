9cf8c27d2f96923e79cef1f4a2aeabb169b00e7c
phamvantam <phamvantam1911@gmail.com>
2023-10-22 20:30:23 +0700

    các cách Restful api web service

--- spring-boot-new/src/main/java/com/tambao/api/NewAPI.java
+++ spring-boot-new/src/main/java/com/tambao/api/NewAPI.java
@@ -1,12 +1,27 @@
 package com.tambao.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tambao.dto.NewDTO;
+
+//@Controller
 @RestController
 public class NewAPI {
-	@GetMapping("/test")
-	public String testAPI() {
-		return "success";
-	}
+//    @RequestMapping(value = "/new", method = RequestMethod.POST)
+//    @ResponseBody
+//    public NewDTO createNew(@RequestBody NewDTO model) {
+//            return model;
+//    }
+	@PostMapping("/new")
+
+    public NewDTO createNew(@RequestBody NewDTO model) {
+            return model;
+    }
 }
+
